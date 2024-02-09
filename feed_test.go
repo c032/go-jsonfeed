3676e92a4efc7b39db1fc86a619275152f174085
c032 <git@c032.dev>
2024-02-09 05:49:14 +0000

    Add tests for `(*Feed).IsExpired()`

--- feed_test.go
+++ feed_test.go
@@ -80,3 +80,33 @@ func TestParse(t *testing.T) {
 		t.Errorf("len(feed.Items) = %d; want %d", len(feed.Items), len(expectedItems))
 	}
 }
+
+func TestFeed_IsExpired_Nil(t *testing.T) {
+	feed := &jsonfeed.Feed{}
+
+	if got, want := feed.IsExpired(), false; got != want {
+		t.Errorf("(&jsonfeed.Feed{}).IsExpired() = %#v; want %#v", got, want)
+	}
+}
+
+func TestFeed_IsExpired_False(t *testing.T) {
+	isExpired := false
+	feed := &jsonfeed.Feed{
+		RawExpired: &isExpired,
+	}
+
+	if got, want := feed.IsExpired(), false; got != want {
+		t.Errorf("feed.IsExpired() = %#v; want %#v", got, want)
+	}
+}
+
+func TestFeed_IsExpired_True(t *testing.T) {
+	isExpired := true
+	feed := &jsonfeed.Feed{
+		RawExpired: &isExpired,
+	}
+
+	if got, want := feed.IsExpired(), true; got != want {
+		t.Errorf("feed.IsExpired() = %#v; want %#v", got, want)
+	}
+}
